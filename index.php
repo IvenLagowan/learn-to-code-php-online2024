798ec3968b9457de4913c1f26d61df1b5ff732d8
Chua <cuaocuq@gmail.com>
2024-01-25 21:35:43 +0900

    turo

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "I like pizza";
+echo "it's really good";
+// this is a comment
+/* This 
+    is 
+    a
+    Multiline
+    comment
+    */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
+
+
+
+
+
+
+
+
---
+++ index2.php
@@ -0,0 +1,21 @@
+<?php
+// Variable = a reusable container that holds data
+//            String, integer, float, boolearn
+
+    $name = "Chua Lagowan";
+    $foot = "Pizza";
+    $email = "cuaocuq@gmail.com";
+
+    $age = 21;
+    $users = 2;
+    $quantity = 3;
+
+    echo "Hello {$name}<br>";
+    echo "Kamu suka {$foot}<br>";
+    echo "Kamu punya email  {$email} <br>";
+
+
+    echo "Kamu tau {$age} Tahun lalu <br>";
+    echo "Kemarin {$users} user online <br>";
+    echo "Kamu sudah tau bayar {$quantity} items<br>";
+?>
